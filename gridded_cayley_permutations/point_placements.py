2edb46eff3decfc2643c24d685e677c116e7c632
Christian Bean <christianbean93@gmail.com>
2025-04-11 11:58:03 +0100

    tidied up point placement

--- gridded_cayley_permutations/point_placements.py
+++ gridded_cayley_permutations/point_placements.py
@@ -1,3 +1,9 @@
+"""
+This module contains the PointPlacement class and its subclasses,
+which includes methods for placing point of a tiling and adding
+obstructions and requirements to make the placements unique.
+"""
+
 from itertools import combinations
 
 from cayley_permutations import CayleyPermutation
@@ -6,27 +12,34 @@ from .gridded_cayley_perms import GriddedCayleyPerm
 from .row_col_map import OBSTRUCTIONS, REQUIREMENTS, RowColMap
 from .tilings import Tiling
 
-Right = 0
-Right_top = 1
-Left_top = 2
-Left = 3
-Left_bot = 4
-Right_bot = 5
+DIR_RIGHT = 0
+DIR_RIGHT_TOP = 1
+DIR_LEFT_TOP = 2
+DIR_LEFT = 3
+DIR_LEFT_BOT = 4
+DIR_RIGHT_BOT = 5
 
-Directions = [Right, Right_top, Left_top, Left, Left_bot, Right_bot]
+Directions = [
+    DIR_RIGHT,
+    DIR_RIGHT_TOP,
+    DIR_LEFT_TOP,
+    DIR_LEFT,
+    DIR_LEFT_BOT,
+    DIR_RIGHT_BOT,
+]
 
 
 class MultiplexMap(RowColMap):
     """
-        A special class that maps
+        A special class for mapping a point onto its own row and column
         + - + - + - +
-        | A |   | A | \
+        | A | A | A | \
         + - + - + - +    + - +
-        |   | o |   | -  | A |
+        | A | o | A | -  | A |
         + - + - + - +    + - +
-        | A |   | A | /
+        | A | A | A | /
         + - + - + - +
-        where the preimage does not place points in the empty cells.
+        It expands a cell to a three by three grid.
         """
 
     def __init__(self, cell: tuple[int, int], dimensions: tuple[int, int]):
@@ -39,7 +52,7 @@ class MultiplexMap(RowColMap):
     def _row_map(self) -> dict[int, int]:
         """Return the row map."""
         row_width = self.dimensions[1] + 2
-        row_map_dict = dict()
+        row_map_dict = {}
         for i in range(row_width):
             if i < self.cell[1]:
                 row_map_dict[i] = i
@@ -64,13 +77,42 @@ class MultiplexMap(RowColMap):
 
 
 class PartialMultiplexMap(MultiplexMap):
+    """A map for mapping a point on to its own column
+    + - + - + - +    + - +
+    | A | o | A | -  | A |
+    + - + - + - +    + - +
+    It expands a cell to a one by three grid.
+    """
+
     def _row_map(self) -> dict[int, int]:
         """Return the row map."""
         return {i: i for i in range(self.dimensions[1])}
 
 
 class PointPlacement:
-    DIRECTIONS = [Right, Right_top, Left_top, Left, Left_bot, Right_bot]
+    """
+    A class for placing points in a tiling
+            + - + - + - +
+            | A |   | A |
+    + - +   + - + - + - +
+    | A | = | A | o | A |
+    + - +   + - + - + - +
+            | A |   | A |
+            + - + - + - +
+    Obstruction and requirements are added to ensure that a unique
+    point is placed onto its own row and column. Additional
+    obstructions are added to ensure that the middle row
+    contains only one value.
+    """
+
+    DIRECTIONS = [
+        DIR_RIGHT,
+        DIR_RIGHT_TOP,
+        DIR_LEFT_TOP,
+        DIR_LEFT,
+        DIR_LEFT_BOT,
+        DIR_RIGHT_BOT,
+    ]
 
     def __init__(self, tiling: Tiling) -> None:
         self.tiling = tiling
@@ -78,8 +120,12 @@ class PointPlacement:
     def point_obstructions_and_requirements(
         self, cell: tuple[int, int]
     ) -> tuple[OBSTRUCTIONS, REQUIREMENTS]:
+        """
+        The obstructions and requirements needed to ensure that the middle cell is a point on
+        its own row and columns, and that the middle row contains only one value.
+        """
         cell = self.placed_cell(cell)
-        x, y = self.new_dimensions(cell)
+        x, y = self.new_dimensions()
         col_obs = [
             GriddedCayleyPerm(CayleyPermutation([0]), [(cell[0], i)])
             for i in range(y)
@@ -114,9 +160,11 @@ class PointPlacement:
         )
 
     def placed_cell(self, cell: tuple[int, int]) -> tuple[int, int]:
+        """Return the cell that has the placed point."""
         return (cell[0] + 1, cell[1] + 1)
 
     def multiplex_map(self, cell: tuple[int, int]) -> MultiplexMap:
+        """Return the multiplex map for the given cell."""
         return MultiplexMap(cell, self.tiling.dimensions)
 
     def forced_obstructions(
@@ -126,6 +174,10 @@ class PointPlacement:
         indices: tuple[int, ...],
         direction: int,
     ) -> OBSTRUCTIONS:
+        """
+        Return the obstructions that are needed to ensure that the placed point is unique.
+        This is the point which is farthest in the given direction at the given indices.
+        """
         multiplex_map = self.multiplex_map(cell)
         cell = self.placed_cell(cell)
         obstructions = []
@@ -138,17 +190,17 @@ class PointPlacement:
     @staticmethod
     def farther(cell1: tuple[int, int], cell2: tuple[int, int], direction: int) -> bool:
         """Return True if cell1 is farther in the given direction than cell2."""
-        if direction == Right:
+        if direction == DIR_RIGHT:
             return cell1[0] > cell2[0]
-        if direction == Right_top:
+        if direction == DIR_RIGHT_TOP:
             return cell1[1] > cell2[1] or (cell1[1] == cell2[1] and cell1[0] > cell2[0])
-        if direction == Left_top:
+        if direction == DIR_LEFT_TOP:
             return cell1[1] > cell2[1] or (cell1[1] == cell2[1] and cell1[0] < cell2[0])
-        if direction == Left:
+        if direction == DIR_LEFT:
             return cell1[0] < cell2[0]
-        if direction == Left_bot:
+        if direction == DIR_LEFT_BOT:
             return cell1[1] < cell2[1] or (cell1[1] == cell2[1] and cell1[0] < cell2[0])
-        if direction == Right_bot:
+        if direction == DIR_RIGHT_BOT:
             return cell1[1] < cell2[1] or (cell1[1] == cell2[1] and cell1[0] > cell2[0])
         raise ValueError(f"Direction {direction} is not valid.")
 
@@ -158,6 +210,9 @@ class PointPlacement:
         indices: tuple[int, ...],
         direction: int,
     ) -> tuple[Tiling, ...]:
+        """
+        Return the tilings that are obtained by placing the points of the gridded permutations
+        in requirement_list in the given direction."""
         if direction not in self.DIRECTIONS:
             raise ValueError(f"Direction {direction} is not a valid direction.")
         cells = []
@@ -176,6 +231,10 @@ class PointPlacement:
         direction: int,
         cell: tuple[int, int],
     ) -> Tiling:
+        """
+        Return the tiling which has placed the point in cell with respect to the given
+        requirement_list and indices.
+        """
         multiplex_map = self.multiplex_map(cell)
         multiplex_obs, multiplex_reqs = multiplex_map.preimage_of_tiling(self.tiling)
         point_obs, point_reqs = self.point_obstructions_and_requirements(cell)
@@ -184,15 +243,20 @@ class PointPlacement:
         )
         obstructions = multiplex_obs + point_obs + forced_obs
         requirements = multiplex_reqs + point_reqs
-        return Tiling(obstructions, requirements, self.new_dimensions(cell))
+        return Tiling(obstructions, requirements, self.new_dimensions())
 
-    def new_dimensions(self, cell=(-1, -1)):
+    def new_dimensions(self) -> tuple[int, int]:
+        """The dimensions of a placed tiling."""
         return (
             self.tiling.dimensions[0] + 2,
             self.tiling.dimensions[1] + 2,
         )
 
     def directionless_point_placement(self, cell: tuple[int, int]) -> Tiling:
+        """
+        Return the tiling obtained by placing the point in the given cell.
+        As this is directionless, the placed point is not necessarily unique.
+        """
         multiplex_map = self.multiplex_map(cell)
         multiplex_obs, multiplex_reqs = multiplex_map.preimage_of_tiling(self.tiling)
         point_obs, point_reqs = self.point_obstructions_and_requirements(cell)
@@ -201,12 +265,21 @@ class PointPlacement:
         return Tiling(
             obstructions,
             requirements,
-            self.new_dimensions(cell),
+            self.new_dimensions(),
         )
 
 
 class PartialPointPlacements(PointPlacement):
-    DIRECTIONS = [Left, Right]
+    """
+    A class for partially placing points in a tiling
+    + - +   + - + - + - +
+    | A | = | A | o | A |
+    + - +   + - + - + - +
+    Obstruction and requirements are added to ensure that a unique
+    point is placed onto its own column.
+    """
+
+    DIRECTIONS = [DIR_LEFT, DIR_RIGHT]
 
     def point_obstructions_and_requirements(
         self, cell: tuple[int, int]

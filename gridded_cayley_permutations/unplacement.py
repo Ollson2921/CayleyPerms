4baf81c20c0d96b74b9820eb94a3afe10bc899de
ReedActon <110858904+ReedActon@users.noreply.github.com>
2025-05-16 13:41:45 +0000

    Full Implimentation

--- gridded_cayley_permutations/unplacement.py
+++ gridded_cayley_permutations/unplacement.py
@@ -1,17 +1,20 @@
+"""The class for point unplacement"""
+
 from itertools import product
-from typing import Iterable
 
 from cayley_permutations import CayleyPermutation
 
 from .gridded_cayley_perms import GriddedCayleyPerm
-from .row_col_map import OBSTRUCTIONS, REQUIREMENTS, RowColMap
 from .tilings import Tiling
 
 Cell = tuple[int, int]
 
 
 class PointUnplacement:
+    """Methods for unplacing points"""
+
     def __init__(self, tiling: Tiling, cell: Cell):
+        assert cell in tiling.point_cells()
         self.tiling = tiling
         self.cell = cell
         self.surrounding_cells = set(
@@ -33,7 +36,8 @@ class PointUnplacement:
     def point_can_be_unplaced(
         self, check_reqs: tuple[tuple[GriddedCayleyPerm, ...], ...]
     ) -> bool:
-        """Checks if the point can be unplaced"""
+        """Checks if the point can be unplaced
+        doesn't fully check fusability across the point row"""
         new_tiling = self.tiling
         if check_reqs:
             if len(check_reqs) > 1:
@@ -50,7 +54,9 @@ class PointUnplacement:
             or self.tiling.dimensions[1] == self.cell[1]
         ):
             return False
-        new_tiling = new_tiling.delete_rows_and_columns((self.cell[0],), (self.cell[1],))
+        new_tiling = new_tiling.delete_rows_and_columns(
+            (self.cell[0],), (self.cell[1],)
+        )
         if not new_tiling.is_fusable(0, self.cell[0] - 1):
             return False
         if not new_tiling.is_fusable(1, self.cell[1] - 1):
@@ -63,6 +69,41 @@ class PointUnplacement:
         intersecting_list = self.intersecting_req_list()[0]
         if intersecting_list:
             new_tiling = self.tiling.remove_requirements(intersecting_list)
-        new_tiling = new_tiling.delete_rows_and_columns((self.cell[0],self.cell[0] +1), (self.cell[1],self.cell[1] +1))
+            adjusted_list = self.adjust_reqs(intersecting_list)
+        else:
+            adjusted_list = [
+                GriddedCayleyPerm(
+                    CayleyPermutation((0,)), [(self.cell[0] - 1, self.cell[1] - 1)]
+                )
+            ]
+        new_tiling = new_tiling.delete_rows_and_columns(
+            (self.cell[0], self.cell[0] + 1), (self.cell[1], self.cell[1] + 1)
+        )
+        new_tiling.add_requirement_list(adjusted_list)
         return new_tiling
 
+    def adjust_reqs(self, req_list: tuple[GriddedCayleyPerm, ...]):
+        """Adds the point to"""
+
+        def cell_correction(cell: Cell) -> Cell:
+            x = cell[0] - int(cell[0] >= self.cell[0]) - int(cell[0] > self.cell[0])
+            y = cell[1] - int(cell[1] >= self.cell[1]) - int(cell[1] > self.cell[1])
+            return (x, y)
+
+        new_req_list = list[GriddedCayleyPerm]()
+        for req in req_list:
+            values = {req.pattern[i]: req.positions[i][1] for i in range(len(req))}
+            value_map = {
+                value: value + int(values[value] > self.cell[1]) for value in values
+            }
+            h_cuttoff = sum(cell[0] < self.cell[0] for cell in req.positions)
+            v_cuttoff = sum(value < self.cell[1] for value in values.values())
+            new_pattern, new_positions = list(req.pattern), list(req.positions)
+            new_pattern.insert(h_cuttoff, v_cuttoff)
+            new_positions.insert(h_cuttoff, self.cell)
+            new_pattern = [value_map[i] for i in new_pattern]
+            new_positions = list(map(cell_correction, new_positions))
+            new_req_list.append(
+                GriddedCayleyPerm(CayleyPermutation(new_pattern), new_positions)
+            )
+        return new_req_list

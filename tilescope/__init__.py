42d11a568eb61001c0a81e6cf409c0069f776e5e
Abigail Ollson <145692399+Ollson2921@users.noreply.github.com>
2025-05-29 11:45:21 +0100

    stopped import errors (#17)

--- tilescope/__init__.py
+++ tilescope/__init__.py
@@ -3,7 +3,7 @@ A package for finding combinatorial specifications
 for Cayley permutation classes
 """
 
-from .tilescope import TileScope
+from .searcher import TileScope
 from .strategy_packs import TileScopePack
 
 __all__ = ("TileScope", "TileScopePack")

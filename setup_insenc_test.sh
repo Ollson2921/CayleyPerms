f6aecedf7478f0f44bc6c1cf4c3ff05d2d8d53af
Christian Bean <christianbean93@gmail.com>
2025-10-08 15:53:15 +0100

    force reinstall in insenc

--- setup_insenc_test.sh
+++ setup_insenc_test.sh
@@ -6,6 +6,7 @@ then
 else
     git clone --depth 1 https://github.com/Ollson2921/cperms_ins_enc.git .insertion_encoding
 fi
+
 cd .insertion_encoding
-pip install .
+pip install --force-reinstall --no-cache-dir .
 cd ..

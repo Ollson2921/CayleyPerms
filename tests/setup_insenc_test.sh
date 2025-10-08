9918cab6fdecb9edafd49bec515694d562fa643f
Abigail Ollson <a.n.ollson@keele.ac.uk>
2025-10-08 11:18:37 +0100

    add ins enc tests to workflow

---
+++ tests/setup_insenc_test.sh
@@ -0,0 +1,11 @@
+if [ -d .insertion_encoding ]
+then
+    cd .insertion_encoding
+    git pull
+    cd ..
+else
+    git clone --depth 1 https://github.com/Ollson2921/cperms_ins_enc.git .insertion_encoding
+fi
+cd .insertion_encoding
+pip install .
+cd ..

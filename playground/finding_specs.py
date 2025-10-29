ff903bf710a998b35e4f77d6a0151fca57a9cb79
Abigail Ollson <a.n.ollson@keele.ac.uk>
2025-10-29 17:16:51 +0000

    more specs

--- playground/finding_specs.py
+++ playground/finding_specs.py
@@ -37,8 +37,8 @@ basis_desc = "3s_4x1" # change descriptor to change file
 with open(f"to_run/to_run_{basis_desc}.txt", "r") as f:
     bases = eval(f.readline())
 
-bases_to_run = bases[2000:2200] # can take a subset of the bases in the file
-part = 18
+bases_to_run = bases[5000:5007] # can take a subset of the bases in the file
+part = 30
 
 counted = set()
 wrong_counts = []

967b147ef7ea8d9cc9c98a22c727e632e123c51c
Abigail Ollson <145692399+Ollson2921@users.noreply.github.com>
2025-11-04 11:23:27 +0000

    Make sure bases are zero based
    Before were allowing bases to be any words, now check bases are 0 based or 1 based and change to 1 based. Raises an error if neither type.

--- cayley_permutations/simplify_basis.py
+++ cayley_permutations/simplify_basis.py
@@ -6,13 +6,35 @@ from typing import Iterable
 from .cayley import CayleyPermutation
 
 
-def string_to_perms(patts: str) -> tuple[CayleyPermutation, ...]:
+def as_zero_based(
+    patts: tuple[CayleyPermutation, ...],
+) -> tuple[CayleyPermutation, ...]:
+    """Turns a tuple of Cayley permutations which are one based or
+    zero based into zero based Cayley permutations.
+    Raises an error if the input is not a valid one based or zero
+    based Cayley permutation."""
+    all_cperms = []
+    for cperm in patts:
+        if cperm.is_cayley_perm():
+            all_cperms.append(cperm)
+        else:
+            one_based_cperm = CayleyPermutation.from_one_based(cperm)
+            if one_based_cperm.is_cayley_perm():
+                all_cperms.append(one_based_cperm)
+            else:
+                raise ValueError(f"The input {cperm} is not a Cayley permutation")
+    return all_cperms
+
+
+def string_to_cperms(patts: str) -> tuple[CayleyPermutation, ...]:
     """
     Construct a tuple of Cayley permutations from a string.
 
     It can be either 0 or 1 based and seperated by anything.
     """
-    return tuple(map(CayleyPermutation.standardise, re.findall(r"\d+", patts)))
+    return as_zero_based(
+        tuple(map(CayleyPermutation.standardise, re.findall(r"\d+", patts)))
+    )
 
 
 def minimise(patts: Iterable[CayleyPermutation]) -> tuple[CayleyPermutation, ...]:
@@ -33,4 +55,4 @@ def string_to_basis(patts: str) -> tuple[CayleyPermutation, ...]:
     """
     Return the minimal patts inputted as a string.
     """
-    return minimise(string_to_perms(patts))
+    return minimise(string_to_cperms(patts))

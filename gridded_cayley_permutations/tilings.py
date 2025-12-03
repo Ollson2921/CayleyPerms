7ae9890b79de04ede5775acd680e3e6caaec630d
Abigail Ollson <145692399+Ollson2921@users.noreply.github.com>
2025-12-03 14:07:31 +0000

    Import combinations from itertools

--- gridded_cayley_permutations/tilings.py
+++ gridded_cayley_permutations/tilings.py
@@ -8,7 +8,7 @@ dimension, that avoid a set of obstructions and contain a set of requirements.
 from collections import defaultdict
 from copy import copy
 from functools import cached_property
-from itertools import chain, product
+from itertools import chain, product, combinations
 from math import factorial
 from typing import Iterable, Iterator
 

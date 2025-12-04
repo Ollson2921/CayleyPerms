8c5abae325d76e0aa7fdd4adc99397f5e4e24647
Abigail Ollson <145692399+Ollson2921@users.noreply.github.com>
2025-12-04 16:16:31 +0000

    proposed new cell insertion factory

--- tilescope/strategies/requirement_insertions.py
+++ tilescope/strategies/requirement_insertions.py
@@ -120,21 +120,116 @@ class HorizontalInsertionEncodingRequirementInsertionFactory(StrategyFactory[Til
         return "Make rows positive"
 
 
+# class CellInsertionFactory(StrategyFactory[Tiling]):
+#     """Factory for inserting points into active cells of a tiling."""
+
+#     def __call__(self, comb_class: Tiling) -> Iterator[RequirementInsertionStrategy]:
+#         for cell in comb_class.active_cells:
+#             gcps = (GriddedCayleyPerm(CayleyPermutation([0]), (cell,)),)
+#             strategy = RequirementInsertionStrategy(gcps, ignore_parent=False)
+#             yield strategy
+
+#     @classmethod
+#     def from_dict(cls, d: dict) -> "CellInsertionFactory":
+#         return cls(**d)
+
+#     def __repr__(self) -> str:
+#         return f"{self.__class__.__name__}()"
+
+#     def __str__(self) -> str:
+#         return "Cell Insertion"
+
+
 class CellInsertionFactory(StrategyFactory[Tiling]):
-    """Factory for inserting points into active cells of a tiling."""
+    """
+    The cell insertion strategy.
+
+    The cell insertion strategy is a disjoint union strategy.
+    For each active cell, the strategy considers all patterns (up to some maximum
+    length given by `maxreqlen`) and returns two tilings; one which requires the
+    pattern in the cell and one where the pattern is obstructed.
+
+    The one_cell_only flag will ensure that the strategy only inserts into the
+    'smallest' non-positive cell. This is used for 'insertion' packs where
+    we are intending to make every cell positive, so with this setting we have
+    a unique path to the fully positive tilings.
+    """
+
+    def __init__(
+        self,
+        maxreqlen: int = 1,
+        extra_basis: Optional[list[CayleyPermutation]] = None,
+        ignore_parent: bool = False,
+        # one_cell_only: bool = False,
+    ) -> None:
+        self.ignore_parent = ignore_parent
+        if extra_basis is None:
+            self.extra_basis = []
+        else:
+            assert isinstance(extra_basis, list)
+            assert all(isinstance(p, CayleyPermutation) for p in extra_basis)
+            self.extra_basis = extra_basis
+        self.maxreqlen = maxreqlen
+        # self.one_cell_only = one_cell_only
+
+    def req_lists_to_insert(
+        self, tiling: Tiling
+    ) -> Iterator[Tuple[GriddedCayleyPerm, ...]]:
+        """Yields all requirement lists to insert into the tiling."""
+        # if self.one_cell_only:
+        #     assert self.maxreqlen == 1 and self.ignore_parent
+        #     cells = sorted(
+        #         frozenset(tiling.active_cells) - frozenset(tiling.positive_cells)
+        #     )
+        #     if cells:
+        #         yield (GriddedPerm.single_cell((0,), cells[0]),)
+        #     return
+        active_cells = tiling.active_cells
+        bdict = tiling.cell_basis()
+        for cell, length in product(active_cells, range(1, self.maxreqlen + 1)):
+            basis = bdict[cell][0] + self.extra_basis
+            patterns = (
+                Av(basis).generate_cperms(length)
+                if basis
+                else CayleyPermutation.of_size(length)
+            )
+            yield from (
+                GriddedCayleyPerm.create_gcp_in_cell(patt, cell)
+                for patt in patterns
+                if not any(patt in cperm for cperm in bdict[cell][1])
+            )
 
     def __call__(self, comb_class: Tiling) -> Iterator[RequirementInsertionStrategy]:
-        for cell in comb_class.active_cells:
-            gcps = (GriddedCayleyPerm(CayleyPermutation([0]), (cell,)),)
-            strategy = RequirementInsertionStrategy(gcps, ignore_parent=False)
-            yield strategy
+        """
+        Iterator over all the requirement insertion rules.
+        """
+        for req_list in self.req_lists_to_insert(comb_class):
+            yield RequirementInsertionStrategy(req_list, self.ignore_parent)
+
+    def to_jsonable(self) -> dict:
+        d: dict = super().to_jsonable()
+        # d["one_cell_only"] = self.one_cell_only
+        return d
 
     @classmethod
     def from_dict(cls, d: dict) -> "CellInsertionFactory":
         return cls(**d)
 
-    def __repr__(self) -> str:
-        return f"{self.__class__.__name__}()"
-
     def __str__(self) -> str:
-        return "Cell Insertion"
+        if self.maxreqlen == 1:
+            return "point insertion"
+        if self.extra_basis:
+            perm_class = Av(self.extra_basis)
+            return f"cell insertion from {perm_class} up to length {self.maxreqlen}"
+        return f"cell insertion up to length {self.maxreqlen}"
+
+    def __repr__(self) -> str:
+        args = ", ".join(
+            [
+                f"maxreqlen={self.maxreqlen}",
+                f"extra_basis={self.extra_basis!r}",
+                f"ignore_parent={self.ignore_parent}",
+                # f"one_cell_only={self.one_cell_only}",
+            ]
+        )
+        return f"{self.__class__.__name__}({args})"

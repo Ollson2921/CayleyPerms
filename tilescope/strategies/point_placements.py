34332c91ddca57bf96f03a3afd21cba064732e9b
Abi <a.n.ollson@keele.ac.uk>
2026-06-04 17:17:07 +0100

    point placements backwards map

--- tilescope/strategies/point_placements.py
+++ tilescope/strategies/point_placements.py
@@ -81,33 +81,26 @@ class AbstractRequirementPlacementStrategy(
         objs: Tuple[Optional[GriddedCayleyPerm], ...],
         children: Optional[Tuple[TilingT, ...]] = None,
     ) -> Iterator[GriddedCayleyPerm]:
-        for obj in objs:
+        placed_cells = [gcp.positions[i] for gcp, i in zip(self.gcps, self.indices)]
+        placed_cells = [(x + 1, y + 1) for x, y in placed_cells]
+        for placed_cell, obj in zip(placed_cells, objs[1:]):
             if obj is None:
                 continue
-            placed_cells = [gcp.positions[i] for gcp, i in zip(self.gcps, self.indices)]
-            placed_cells = [(x + 1, y + 1) for x, y in placed_cells]
             new_positions = []
             for cell in obj.positions:
-                placed_cells_below_x = sum(
-                    2 for placed_cell in placed_cells if placed_cell[0] < cell[0]
+                adjust_cols = 2 if placed_cell[0] < cell[0] else 0
+                adjust_cols += (
+                    1
+                    if any(cell[0] == placed_cell[0] for placed_cell in placed_cells)
+                    else 0
                 )
-                placed_cells_below_y = sum(
-                    2 for placed_cell in placed_cells if placed_cell[1] < cell[1]
+                adjust_rows = 2 if placed_cell[1] < cell[1] else 0
+                adjust_rows += (
+                    1
+                    if any(cell[1] == placed_cell[1] for placed_cell in placed_cells)
+                    else 0
                 )
-                new_cell = (
-                    cell[0]
-                    - placed_cells_below_x
-                    - 1
-                    * int(
-                        any(cell[0] == placed_cell[0] for placed_cell in placed_cells)
-                    ),
-                    cell[1]
-                    - placed_cells_below_y
-                    - 1
-                    * int(
-                        any(cell[1] == placed_cell[1] for placed_cell in placed_cells)
-                    ),
-                )
+                new_cell = (cell[0] - adjust_cols, cell[1] - adjust_rows)
                 new_positions.append(new_cell)
             yield GriddedCayleyPerm(obj.pattern, tuple(new_positions))
 

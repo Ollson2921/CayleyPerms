0a4845cecb54034a7c598eb54aba78dbaf90f106
Abi <a.n.ollson@keele.ac.uk>
2026-06-09 16:02:42 +0100

    not yet implemented

--- tilescope/strategies/factor.py
+++ tilescope/strategies/factor.py
@@ -41,6 +41,7 @@ class AbstractFactorStrategy(CartesianProductStrategy[TilingT, GriddedCayleyPerm
         objs: Tuple[Optional[GriddedCayleyPerm], ...],
         children: Optional[Tuple[TilingT, ...]] = None,
     ) -> Iterator[GriddedCayleyPerm]:
+        raise NotImplementedError
         temp = [
             ((cell[0], idx), (cell[1], val))
             for gcp in objs
@@ -49,7 +50,8 @@ class AbstractFactorStrategy(CartesianProductStrategy[TilingT, GriddedCayleyPerm
         ]
         temp.sort()
         new_positions = [(idx[0], val[0]) for idx, val in temp]
-        new_pattern = CayleyPermutation.standardise([val for _, val in temp])
+        # new_pattern = CayleyPermutation.standardise([val for _, val in temp])
+
         yield GriddedCayleyPerm(new_pattern, tuple(new_positions))
 
     def forward_map(
